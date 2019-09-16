4d3249902359ad950f2106996de0c4c86871b537
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2019-09-16 12:24:15 +0300

    Initial commit

---
+++ precache-manifest.5eb79c49bba47ca3927f8d629d8a02b6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0892dfc44d53c7b51db387271dfd6ed1",
+    "url": "/task-manager/img/edit.0892dfc4.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/task-manager/robots.txt"
+  },
+  {
+    "revision": "988a293294c2a84dc8d04dd9854708f1",
+    "url": "/task-manager/img/arrow_back_ios.988a2932.svg"
+  },
+  {
+    "revision": "dda0a3e61fec91cbc285",
+    "url": "/task-manager/css/chunk-vendors.0c8151db.css"
+  },
+  {
+    "revision": "efc0c7c4086522795f7e5d318e2f41e2",
+    "url": "/task-manager/img/floppy-o.efc0c7c4.svg"
+  },
+  {
+    "revision": "5a3498d45fbe829330bbd1c9cc18e0cb",
+    "url": "/task-manager/img/spinner.5a3498d4.gif"
+  },
+  {
+    "revision": "543ec936b6c8a4ac8ecd",
+    "url": "/task-manager/js/app.1030984d.js"
+  },
+  {
+    "revision": "dda0a3e61fec91cbc285",
+    "url": "/task-manager/js/chunk-vendors.c1fe8b23.js"
+  },
+  {
+    "revision": "3fd199890bc0e138b8e575de8cc0836a",
+    "url": "/task-manager/img/trash-o.3fd19989.svg"
+  },
+  {
+    "revision": "0cb9a450cfdbb331b415adf2ac3f5fd9",
+    "url": "/task-manager/img/arrows.0cb9a450.svg"
+  },
+  {
+    "revision": "104aa2a7b9662b7cf7c226bab34bf171",
+    "url": "/task-manager/img/arrow_forward_ios.104aa2a7.svg"
+  },
+  {
+    "revision": "fdefbf8a07cc38a400aefb2143884e8f",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "543ec936b6c8a4ac8ecd",
+    "url": "/task-manager/css/app.a164243d.css"
+  }
+];
